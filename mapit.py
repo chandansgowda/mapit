70b871313950c25cfee2c8d3157c69c1de1505d1
Chandan Gowda <chandansuresh007@gmail.com>
2020-10-23 19:33:33 +0530

    Described each step using comments

--- mapit.py
+++ mapit.py
@@ -1,5 +1,5 @@
 import webbrowser
-
+#importing required library
 
 def map1(latitude, longitude):
       webbrowser.open(f'https://www.google.com/maps/place/{latitude} {longitude}')
@@ -9,12 +9,15 @@ def map2(address):
       webbrowser.open(f'https://www.google.com/maps/place/{address}')
       print("You are being redirected to Google Maps")
 
+#defined required functions
 
 print('''
 Select any option:
       1.Address
       2.Latitude and Longitude
 ''')
+#question output
+
 selection = int(input('>>   '))
 if selection == 1 :
       address = input("Give the address [with spaces] >> ")
@@ -27,4 +30,4 @@ elif selection == 2 :
 
 else :
       print("That was a wrong input.Try again!")
-
+#program ends
